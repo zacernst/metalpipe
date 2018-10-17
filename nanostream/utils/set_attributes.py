92b0c0c1913e3f787906a5f84b3641cc3389e09b
zac.ernst <zernst@civisanalytics.com>
2018-10-17 17:34:21 +0000

    decorators,etc

--- nanostream/utils/set_attributes.py
+++ nanostream/utils/set_attributes.py
@@ -2,12 +2,42 @@
 Decorator that eliminates the need to assign attributes to the keys
 and values of kwargs in an ``__init__`` function.
 '''
+
 import inspect
 
 
 class set_kwarg_attributes:
+    '''
+    Function decorator to be used on ``__init__`` methods. It examines the
+    signature of the ``__init__`` method and any kwargs passed to the
+    constructor. It creates attributes named after the keys, and assigns
+    the corresponding values to those attributes.
+
+    For example:
+
+    ::
+
+        class Foo:
+            @set_kwarg_attributes()
+            def __init__(self, foo='bar', bar='baz'):
+                pass
+
+    Calling ``foo = Foo()``, you'd find that ``foo.foo == 'bar`` and
+    ``foo.bar == 'baz'``, without having to go through the tedious torture
+    of explicitly writing boilerplate like ``self.foo = foo``.
+
+    You can also exclude some kwargs from this magic by passing a list
+    ``exclude=[...]`` to the decorator.
+    '''
 
     def __init__(self, exclude=None):
+        '''
+        Constructor for the decorator.
+
+        Args:
+            exclude (list of str): Names of kwargs that will **not** be
+                assigned to attributes of the object.
+        '''
         self.exclude = exclude or []
 
     def __call__(self, f):
@@ -23,18 +53,3 @@ class set_kwarg_attributes:
                 setattr(_self, kwarg, value)
             f(_self, *args, **kwargs)
         return inner_function
-
-
-if __name__ == '__main__':
-    # Example usage
-
-    class Foo:
-
-        @set_kwarg_attributes()
-        def __init__(self, thing, foo='bar', baz='qux'):
-            print('hi')
-
-        def whatever(self):
-            return 'hi'
-
-    foo = Foo('hithere', foo='baz')
--- nanostream/utils/timed_events.py
+++ nanostream/utils/timed_events.py
@@ -1,19 +1,44 @@
 import threading
 import time
+from nanostream.utils.required_arguments import required_arguments
 
 
-def make_timer(interval=None):
-    event_obj = threading.Event()
+class RepeateEvent:
 
-    def event_loop():
-        while 1:
-            time.sleep(interval)
-            event_obj.set()
-            while event_obj.is_set():
-                pass
+    @required_arguments('interval')
+    def __init__(self, daemon=True, interval=None, number_of_events=None, start=True):
+        self.event_obj = threading.Event()
+        self.run = start
 
-    event_thread = threading.Thread(target=event_loop, daemon=True)
-    return event_thread, event_obj
+        def event_loop():
+            while kill_switch.run:
+                time.sleep(interval)
+                self.event_obj.set()
+                while self.event_obj.is_set():
+                    pass
 
-event_thread, event_obj = make_timer()
+        self.event_thread = threading.Thread(target=event_loop, daemon=daemon)
+        if self.run:
+            self.event_thread.start()
+
+    def start(self):
+        self.event_thread.start()
+
+    def kill(self):
+        self.run = False
+
+
+
+
+event_thread, event_obj, kill_switch = make_timer(interval=2)
+event_thread.start()
+counter = 0
+while 1:
+    print('hi: ' + str(counter))
+    counter += 1
+    while not event_obj.is_set():
+        pass
+    event_obj.clear()
+    if counter > 3:
+        kill_switch.kill()
 
