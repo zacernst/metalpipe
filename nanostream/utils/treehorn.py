3100dd6540839946ba2edaf8b0299b849481b213
zac.ernst <zernst@civisanalytics.com>
2019-02-07 22:44:11 +0000

    treehorn

--- nanostream/utils/treehorn.py
+++ nanostream/utils/treehorn.py
@@ -1,3 +1,4 @@
+import types
 from nanostream.utils.helpers import *
 
 
@@ -7,39 +8,129 @@ class TreeFinder:
 
 class MeetsCondition(TreeFinder):
 
-    def __init__(self, test_function=None, direction='down', **kwargs):
+    def __init__(self, test_function=None, truth_value=True, direction='down', **kwargs):
         '''
         Fix generator in __call__ so that it depends on whether the argument
         is a generator or a TracedObject.
         '''
         self.test_function = test_function or (lambda x: True)
         self.direction = direction
+        self.truth_value = truth_value
         super(MeetsCondition, self).__init__(**kwargs)
+
+    def _test(self, node):
+        return self.test_function(node.value)
+
+    @staticmethod
+    def _trivial_generator(thing):
+        yield thing
+
+    def __call__(self, thing):
+        thing = MeetsCondition._trivial_generator(thing) if not isinstance(
+            thing, (types.GeneratorType,)) else thing
+        for outer_node in thing:
+            generator = (
+                outer_node.descendants if self.direction == 'down'
+                else outer_node.ancestors)
+            for node in generator:
+                if self.test_function(node) == self.truth_value:
+                    yield node
+
+    def __and__(self, other):
+        return And(self, other)
+
+    def __invert__(self):
+        return Not(self)
+
+    def __or__(self, other):
+        return ~ (~ self & ~ other)
+
+    def __eq__(self, other):
+        return (self & other) | (~ self & ~ other)
+
+    def __ne__(self, other):
+        return ~ (self == other)
+
+
+class HasDescendantOrAncestor(MeetsCondition):
+
+    def __init__(self, condition_object=None, **kwargs):
+        self.condition_object = condition_object
+        super(HasDescendantOrAncestor, self).__init__(**kwargs)
 
     def __call__(self, thing):
         generator = (
-            thing.descendants if self.direction == 'down' else thing.ancestors)
-        if isinstance(self.test_function, (TreeFinder,)):
-            out = [
-                thing for thing in generator if self.test_function(thing.value)]
-        else:
-            out = [thing for thing in generator if self.test_function(thing)]
-        for one_thing in out:
-            yield one_thing
+            thing.descendants if isinstance(self, (HasDescendant,)) else
+            thing.ancestors)
+        for outer_node in generator:
+            sentinal = False
+            for _ in self.condition_object(outer_node):
+                sentinal = True
+                break
+            if sentinal:
+                yield outer_node
+
+
+class HasDescendant(HasDescendantOrAncestor):
+
+    def __init__(self, condition_object=None, **kwargs):
+        super(HasDescendant, self).__init__(
+            condition_object=condition_object, **kwargs)
+
+
+class HasAncestor(HasDescendantOrAncestor):
+
+    def __init__(self, condition_object=None, **kwargs):
+        super(HasAncestor, self).__init__(
+            condition_object=condition_object, **kwagrs)
+
+
+class HasKey(MeetsCondition):
+    def __init__(self, key=None, **kwargs):
+        self.key = key
+
+        def _condition(thing):
+            return isinstance(thing, (TracedDictionary,)) and self.key in thing
+
+        super(HasKey, self).__init__(test_function=_condition, **kwargs)
+
+
+class And(MeetsCondition):
+
+    def __init__(self, conjunct_1, conjunct_2, **kwargs):
+        self.conjunct_1 = conjunct_1
+        self.conjunct_2 = conjunct_2
+
+        def _test_function(node):
+            return (
+                self.conjunct_1.test_function(node) and
+                self.conjunct_2.test_function(node))
+
+        super(And, self).__init__(test_function=_test_function, **kwargs)
+
+
+class Not(MeetsCondition):
+
+    def __init__(self, condition, **kwargs):
+        self.condition = condition
+
+        def _test_function(node):
+            return not self.condition.test_function(node)
+
+        super(Not, self).__init__(test_function=_test_function, **kwargs)
 
 
 class IsList(MeetsCondition):
 
     def __init__(self, direction='down', **kwargs):
         super(IsList, self).__init__(
-            direction=direction,
-            test_function=(lambda x: isinstance(x, (TracedList,))))
+            test_function=(lambda x: isinstance(x, (TracedList,))), **kwargs)
 
 
 class IsDictionary(MeetsCondition):
 
     def __init__(self, direction='down', **kwargs):
-        super(IsList, self).__init__(
+        super(IsDictionary, self).__init__(
             direction=direction,
             test_function=(lambda x: isinstance(x, (TracedDictionary,))))
 
@@ -129,6 +220,9 @@ class TracedList(TracedObject, list):
             self.children.append(child)
             self.append(child)
 
+    def __repr__(self):
+        return ', '.join([item.__repr__() for item in self])
+
 
 def splitter(
     thing,
@@ -178,17 +272,31 @@ class TracedDictionary(TracedObject, dict):
             self.children.append(child)
 
 
-
-
 if __name__ == '__main__':
     d = {
         'foo': 'bar',
         'bar': 'baz',
         'baz': {'foobar': 1, 'goober': 2},
-        'qux': ['foo', 'foobarbaz', 'ding', {'goo': 'blergh'}]}
+        'qux': ['foo', 'foobarbaz', 'ding', {'goo': 'blergh'}],
+        'a': {'b': {'c': 'd', 'some_list': [1, 2, 4,]}},
+        'a1': {'b1': {'c': 'd', 'some_list': [1, 2, 4,], 'e': 'whatever'}}}
     thing = splitter(d)
     print(thing['qux'][3].root)
     c = MeetsCondition(
         test_function=(
             lambda x: isinstance(
                 x, (TracedPrimitive,)) and x.thing == 'blergh'))
+    print(list(c(thing)))
+    c = IsList(thing)
+    print(list(c(thing)))
+    c = HasKey(key='goo')
+    print(list(c(thing)))
+    has_goober = HasKey(key='goober')
+    has_foobar = HasKey(key='foobar')
+    conjunction = And(has_goober, has_foobar)
+    print(list(conjunction(thing)))
+    not_list = Not(IsList())
+    print(list(not_list(thing)))
+    d_test = HasDescendant(IsDictionary())
+    print('---')
+    print(list(d_test(thing)))
